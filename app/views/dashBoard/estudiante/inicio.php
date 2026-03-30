6738ec892fa93168b792febd49bf5d3dc1956bbe
Yair <yandrey2007@gmail.com>
2026-03-30 12:35:57 -0500

    Cambiando el enrutamiento de todos los archivos a php

--- app/views/dashBoard/estudiante/inicio.php
+++ app/views/dashBoard/estudiante/inicio.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Dashboard Estudiante — Colegio San Cristóbal</title>
-  <link rel="stylesheet" href="../../../../public/assets/dashBoard/estudiante/css/inicio.css" />
+  <link rel="stylesheet" href="<?= BASE_URL ?>/public/assets/dashBoard/estudiante/css/inicio.css" />
   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 </head>
@@ -530,6 +530,6 @@
     </div>
   </main>
 
-  <script src="../../../../public/assets/dashBoard/estudiante/js/inicio.js"></script>
+  <script src="<?= BASE_URL ?>/public/assets/dashBoard/estudiante/js/inicio.js"></script>
 </body>
 </html>
